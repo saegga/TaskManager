062b0c637ced3373268a7e7802260d6b6bbf4c5e
saegga <sergei.muzalev94@mail.ru>
2016-08-30 17:33:53 +0300

    add app setup db method

--- app/src/main/java/sergei/taskmanager/App.java
+++ app/src/main/java/sergei/taskmanager/App.java
@@ -1,6 +1,10 @@
 package sergei.taskmanager;
 
 import android.app.Application;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.generator.greendao.DaoMaster;
+import com.generator.greendao.DaoSession;
 
 /**
  * Created by sergei on 30.08.2016.
@@ -8,14 +12,28 @@ import android.app.Application;
 
 public class App extends Application {
 
-
+    DaoMaster.DevOpenHelper openHelper;
+    SQLiteDatabase database;
+    DaoMaster daoMaster;
+    DaoSession daoSession;
     @Override
     public void onCreate() {
         super.onCreate();
-
+        setupDataBase();
     }
 
     private void setupDataBase(){
-        
+
+        openHelper = new DaoMaster.DevOpenHelper(this, "TASK", null);
+        database = openHelper.getWritableDatabase();
+        daoMaster = new DaoMaster(database);
+        daoSession = daoMaster.newSession();
+    }
+
+    public DaoSession getDaoSession(){
+        return daoSession;
+    }
+    public SQLiteDatabase getDataBase(){
+        return database;
     }
 }
