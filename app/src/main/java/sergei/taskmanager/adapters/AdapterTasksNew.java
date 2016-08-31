842afdff883fb6aa79344a5623e0543faba618dc
saegga <sergei.muzalev94@mail.ru>
2016-08-31 06:33:06 +0300

    add adapter for recycler view

---
+++ app/src/main/java/sergei/taskmanager/adapters/AdapterTasksNew.java
@@ -0,0 +1,51 @@
+package sergei.taskmanager.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.generator.greendao.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import sergei.taskmanager.R;
+
+/**
+ * Created by sergei on 31.08.2016.
+ */
+public class AdapterTasksNew extends RecyclerView.Adapter<AdapterTasksNew.ViewHolder> {
+
+    private List<Task> tasks;
+
+    public AdapterTasksNew(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    @Override
+    public AdapterTasksNew.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_new_task_item, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(AdapterTasksNew.ViewHolder holder, int position) {
+        holder.textTask.setText(tasks.get(position).getText_task());
+    }
+
+    @Override
+    public int getItemCount() {
+        return tasks.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView textTask;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            textTask = (TextView) itemView.findViewById(R.id.text_new_task);
+        }
+    }
+}
--- app/src/main/java/sergei/taskmanager/fragment/TaskManagerFragment.java
+++ app/src/main/java/sergei/taskmanager/fragment/TaskManagerFragment.java
@@ -4,15 +4,22 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.generator.greendao.DaoSession;
+import com.generator.greendao.Task;
 import com.generator.greendao.TaskDao;
+
+import java.util.List;
 
 import sergei.taskmanager.App;
 import sergei.taskmanager.R;
+import sergei.taskmanager.adapters.AdapterTasksNew;
 
 /**
  * Created by sergei on 28.08.2016.
@@ -20,22 +27,43 @@ import sergei.taskmanager.R;
 public class TaskManagerFragment extends Fragment {
 
     private FloatingActionButton fabAddTask;
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private List<Task> taskList;
+    private TextView emptyView;
+    private DaoSession daoSession;
+    private TaskDao taskDao;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.task_layout_fragment, container, false);
         fabAddTask = (FloatingActionButton) view.findViewById(R.id.fab_add_new_task);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view_new_task);
+        emptyView = (TextView) view.findViewById(R.id.empty_view_tasks);
+        layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
         return view;
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         //// TODO: 30.08.2016 Добавить EventBus
-//        session = ((App) getActivity().getApplication()).getDaoSession();
-//        taskDao = session.getTaskDao();
-//        taskDao.loadAll();
+        daoSession = ((App) getActivity().getApplication()).getDaoSession();
+        taskDao = daoSession.getTaskDao();
+        taskList = taskDao.loadAll();
+        setViewRecycler(taskList);
 
+    }
+    public void setViewRecycler(List<Task> taskList){
+        AdapterTasksNew adapterTasksNew = new AdapterTasksNew(taskList);
+        if(adapterTasksNew.getItemCount() == 0){
+            emptyView.setVisibility(View.VISIBLE);
+            recyclerView.setVisibility(View.GONE);
+        }else{
+            recyclerView.setAdapter(adapterTasksNew);
+            emptyView.setVisibility(View.GONE);
+            recyclerView.setVisibility(View.VISIBLE);
+        }
     }
 }
