a7affbcc4aaa1bea1a53e1899bb835163a0bb667
saegga <sergei.muzalev94@mail.ru>
2016-08-31 06:01:31 +0300

    add layout recycler and fab

--- app/src/main/java/sergei/taskmanager/fragment/TaskManagerFragment.java
+++ app/src/main/java/sergei/taskmanager/fragment/TaskManagerFragment.java
@@ -2,11 +2,16 @@ package sergei.taskmanager.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.generator.greendao.DaoSession;
+import com.generator.greendao.TaskDao;
+
+import sergei.taskmanager.App;
 import sergei.taskmanager.R;
 
 /**
@@ -14,15 +19,23 @@ import sergei.taskmanager.R;
  */
 public class TaskManagerFragment extends Fragment {
 
+    private FloatingActionButton fabAddTask;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.task_layout_fragment, container, false);
+        fabAddTask = (FloatingActionButton) view.findViewById(R.id.fab_add_new_task);
         return view;
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //// TODO: 30.08.2016 Добавить EventBus
+//        session = ((App) getActivity().getApplication()).getDaoSession();
+//        taskDao = session.getTaskDao();
+//        taskDao.loadAll();
+
     }
 }
--- app/src/test/java/sergei/taskmanager/ExampleUnitTest.java
+++ app/src/test/java/sergei/taskmanager/ExampleUnitTest.java
@@ -1,6 +1,14 @@
 package sergei.taskmanager;
 
+import android.app.Application;
+
+import com.generator.greendao.DaoSession;
+import com.generator.greendao.Task;
+import com.generator.greendao.TaskDao;
+
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -11,5 +19,13 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void daoTest(){
+//        App app = new App();
+//        DaoSession session = app.getDaoSession();
+//        TaskDao taskDao = session.getTaskDao();
+//        List<Task> tasks = taskDao.loadAll();
+//        assertNull(tasks);
     }
 }
